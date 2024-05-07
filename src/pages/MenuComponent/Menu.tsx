ca4fbdfa6400f9154db09775990c3896098de76c
devemit <mitkoiliev505@gmail.com>
2024-05-07 20:49:11 +0200

    /

--- src/pages/MenuComponent/Menu.tsx
+++ src/pages/MenuComponent/Menu.tsx
@@ -5,14 +5,14 @@ const Menu = () => {
   return (
     <div className='menu_container'>
       <MenuCard
-        imgUrl='https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-        label={'FOODS'}
-        link='/foods'
-      />
-      <MenuCard
         imgUrl='https://images.pexels.com/photos/1189261/pexels-photo-1189261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
         label={'DRINKS'}
         link='/drinks'
+      />
+      <MenuCard
+        imgUrl='https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        label={'FOODS'}
+        link='/foods'
       />
     </div>
   );
