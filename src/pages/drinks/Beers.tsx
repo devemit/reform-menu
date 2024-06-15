66f1e2f3f6e7a36256441aa57bd2e433293aa938
devemit <mitkoiliev505@gmail.com>
2024-06-15 08:50:12 +0200

    Price change

--- src/pages/drinks/Beers.tsx
+++ src/pages/drinks/Beers.tsx
@@ -41,7 +41,7 @@ const Beers = () => {
         </tr>
         <tr>
           <td>Точено големо/Pint big</td>
-          <td>120ден/2€</td>
+          <td>110ден/2€</td>
         </tr>
       </table>
     </div>
