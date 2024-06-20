a796e607f70941d4eb1ca23d73bc278cc90194c2
devemit <mitkoiliev505@gmail.com>
2024-06-20 13:34:50 +0200

    /

--- src/pages/MenuComponent/ImageComponent.tsx
+++ src/pages/MenuComponent/ImageComponent.tsx
@@ -1,29 +1,16 @@
-import { useState, useEffect } from 'react';
-import { Blurhash } from 'react-blurhash';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 export default function ImageComponent({ src }: any) {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.onload = () => {
-      setImageLoaded(true);
-    };
-    img.src = src;
-  }, []);
-
   return (
-    <>
-      {!imageLoaded && (
-        <Blurhash
-          hash='LEHV6nWB2yk8pyo0adR*.7kCMdnj'
-          width={400}
-          height={220}
-          resolutionX={32}
-          resolutionY={32}
-          punch={1}
-        />
-      )}
-      {imageLoaded && <img loading='lazy' src={src} className='menu-card_image' />}
-    </>
+    <LazyLoadImage
+      loading='lazy'
+      src={src}
+      className='menu-card_image'
+      visibleByDefault={true}
+      width={500}
+      height={400}
+      effect='blur'
+      threshold={100}
+    />
   );
 }
