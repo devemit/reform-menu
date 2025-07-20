f312e116a938616554606ef2b9846ca739e518bc
Mitko <mitkoiliev505@gmail.com>
2025-07-20 21:00:12 +0200

    add: madeby

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,28 @@ export default function Home() {
       <Menu />
       <Capacity />
       <Events />
+      <div
+        style={{
+          bottom: '2px',
+          padding: '4px 20px',
+          borderRadius: '3px',
+          left: '5px',
+          cursor: 'pointer',
+          fontSize: '10px',
+        }}
+      >
+        <a
+          style={{
+            textDecoration: 'none',
+            color: 'blue',
+          }}
+          href='https://mitkodev.vercel.app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          © 2025 Mitko Devemit
+        </a>
+      </div>
     </div>
   );
 }
