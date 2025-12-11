9444e4bc5ff0cb0b07a1af49aecf03df2787cc80
Mitko <mitkoiliev505@gmail.com>
2025-12-11 20:32:47 +0100

    add: snowfall effect & remove promo component

--- src/App.tsx
+++ src/App.tsx
@@ -23,12 +23,14 @@ import Sandwiches from './pages/foods/Sandwiches';
 import Deserts from './pages/foods/Deserts';
 import Appetizers from './pages/foods/Appetizers';
 import Home from './pages/Home';
+import Snowfall from 'react-snowfall'
 
 const App = () => {
   return (
     <LanguageProvider>
+      <Snowfall color='white' />
       <MainHeader />
-      <PromoTooltip showDelayMs={0} />
+      {/*<PromoTooltip showDelayMs={0} />*/}
       <Routes>
         {/* foods */}
         <Route path='/' element={<Home />} />
