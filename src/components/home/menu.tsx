1225eb1972662bfd14c4015216c4fd38bd07cdef
Mitko <mitkoiliev505@gmail.com>
2026-04-18 22:20:14 +0200

    feat: add Menu component with navigation links for drinks and foods

---
+++ src/components/home/menu.tsx
@@ -0,0 +1,31 @@
+import { NavLink } from 'react-router-dom';
+import ImageComponent from './image-component';
+import { useLanguage } from '../../contexts/language-context';
+import './menu.css';
+
+const Menu = () => {
+  const { t } = useLanguage();
+
+  return (
+    <div className='menu_container'>
+      <div className='men'>
+        <div className='blur-image'>
+          <ImageComponent src={'/cocc.jpg'} />
+        </div>
+        <NavLink className='link' to='/drinks'>
+          <button className='btn_link'>{t('menu.drinks')}</button>
+        </NavLink>
+      </div>
+      <div className='men'>
+        <div className='blur-image'>
+          <ImageComponent src={'/hamm.jpg'} />
+        </div>
+        <NavLink className='link' to='/foods'>
+          <button className='btn_link'>{t('menu.food')}</button>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
